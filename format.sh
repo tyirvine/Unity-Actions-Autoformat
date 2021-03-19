370d1bf85fbc4b87d5661af3bec915d64d9d72dc
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2021-03-19 17:15:01 -0600

    Trying to install dotnet-format

--- format.sh
+++ format.sh
@@ -8,18 +8,19 @@ token=$2
 if [ -d $path ]; then
     echo "$path exists"
 
+    # Install dotnet format
+    dotnet tool install -g dotnet-format
+
+    # Format files in folder
+
+    # Check for changes
+
+    # Commit
+
+    # Push
+
 else
     echo "$path does not exist"
-
-# Install dotnet format
-
-# Format files in folder
-
-# Check for changes
-
-# Commit
-
-# Push
 
 
 fi
